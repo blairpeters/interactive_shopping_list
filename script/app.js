d0640e00f01081e4a0f27df351227a10b423f77a
Tony Blair Maina <blairpeters13@gmail.com>
2024-07-10 16:16:55 +0300

    1. Add utility functions 2.Add DomContentLoaded event listener 3.Add Functions to display items.

---
+++ script/app.js
@@ -0,0 +1,52 @@
+//creating an element
+// const paragraph = document.createElement("");
+// const h1 = document.createElement("");
+// const div = document.createElement("");
+// const h2 = document.createElement("");
+
+//to avoid the above repetion you can just create a function that creates an element
+
+function createAnElement(element) {
+  return document.createElement(element);
+}
+
+// const h4 = createAnElement("h4");
+
+// // creating an inner text on the div
+// h2.innerText = "i am a h2 tag";
+// h1.innerText = "i am a h1 tag";
+// div.innerText = "i am a div tag";
+
+function addText(element, text) {
+  return (element.innerText = text);
+}
+
+function appendChild(child, parent) {
+  return parent.appendChild(child);
+}
+
+function select(selector) {
+  return document.querySelector(selector);
+}
+
+function listen(element, event, callback) {
+  return element.addEventListener(event, callback);
+}
+
+function addAttribute(element, attribute, content) {
+  return element.setAttribute(attribute, content);
+}
+
+const shoppingList = ["milk", "tea"];
+
+const ol = select("ol");
+listen(document, "DOMContentLoaded", displayItems);
+
+function displayItems() {
+  shoppingList.forEach(createAListItem);
+}
+function createAListItem(item) {
+  const li = createAnElement("li");
+  addText(li, item);
+  appendChild(li, ol);
+}
